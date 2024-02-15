76091b130412afab5c2a2b5619483162a265d26f
noelhoppe <noel.hoppe@gmx.de>
2024-02-15 18:06:12 +0100

    small improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,9 @@ module.exports = {
       "index.html"
   ],
   theme: {
-    extend: {
-      backgroundColor: {
-        "main-theme-1" : "hsl(222, 26%, 31%)",
-        "keypad-theme-1": "hsl(223, 31%, 20%)",
-        "screen-theme-1" : "hsl(224, 36%, 15%)",
-        "key-background-blue-theme-1": "hsl(225, 21%, 49%)",
-        "key-background-red-theme-1": "hsl(6, 63%, 50%)",
-        "key-background-orange-theme-1": "hsl(30, 25%, 89%)"
-      },
-      textColor: {
-        "clr-theme-1" : "hsl(221, 14%, 31%)"
-      },
-      boxShadowColor: {
-        "key-shadow-red-theme-1": "hsl(6, 70%, 34%)",
-        "key-shadow-keys-theme-1": "hsl(28, 16%, 65%)",
-        "key-shadow-blue-theme-1": "hsl(224, 28%, 35%)"
-      }
-    },
+    fontFamily: {
+      sans : ["League Spartan", "sans-serif"]
+    }
   },
   plugins: [],
 }
